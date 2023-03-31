98b8a7edd795193df76195f0fc8cbbe233dd90c2
reddangerous <dkinyanjui144@gmail.com>
2023-03-31 13:35:54 +0300

    Updated Lyrics

--- lyrics.js
+++ lyrics.js
@@ -49,82 +49,83 @@ function loadLyricsTable() {
         });
       });
 
-      // Add event listener to the edit button
-const editButton = document.querySelector('#edit-lyrics-button');
-editButton.addEventListener('click', () => {
-  const selectedRow = document.querySelector('#lyrics-table tbody .table-primary');
-  if (selectedRow) {
-    const id = selectedRow.dataset.id;
-    const musicTitle = selectedRow.querySelector('.music-title').textContent;
-    const artist = selectedRow.querySelector('.artist').textContent;
-    const lyrics = selectedRow.querySelector('.lyrics').textContent;
-    
-    // Populate form with selected lyric data
-    document.querySelector('#edit-lyric-id').value = id;
-    document.querySelector('#edit-music-title').value = musicTitle;
-    document.querySelector('#edit-artist').value = artist;
-    document.querySelector('#edit-lyrics').value = lyrics;
+ // Add event listener to the edit button
+      const editButton = document.querySelector('#edit-lyrics-button');
+      editButton.addEventListener('click', () => {
+        const selectedRow = document.querySelector('#lyrics-table tbody .table-primary');
+        if (selectedRow) {
+          const id = selectedRow.dataset.id;
+          const musicTitle = selectedRow.querySelector('.music-title').textContent;
+          const artist = selectedRow.querySelector('.artist').textContent;
+          const lyrics = selectedRow.querySelector('.lyrics').textContent;
+          
+          // Populate form with selected lyric data
+          document.querySelector('#edit-lyric-id').value = id;
+          document.querySelector('#edit-music-title').value = musicTitle;
+          document.querySelector('#edit-artist').value = artist;
+          document.querySelector('#edit-lyrics').value = lyrics;
+  
+          // Show the edit lyric form
+          document.querySelector('#edit-lyric-form').classList.remove('d-none');
+        } else {
+          alert('Please select a row to edit.');
+        }
+      });
 
-    // Show the edit lyric form
-    document.querySelector('#edit-lyric-form').classList.remove('d-none');
-  } else {
-    alert('Please select a row to edit.');
-  }
-});
-
-// Add event listener to the edit lyric form
-const editLyricForm = document.querySelector('#edit-lyric-form');
-editLyricForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form from submitting via browser
-  const id = document.querySelector('#edit-lyric-id').value;
-  const musicTitle = document.querySelector('#edit-music-title').value;
-  const artist = document.querySelector('#edit-artist').value;
-  const lyrics = document.querySelector('#edit-lyrics').value;
-
-  // Send PUT request to server to update lyric data
-  fetch(`https://allyricsbackend-production.up.railway.app/lyrics/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      music_title: musicTitle,
-      artist: artist,
-      lyrics: lyrics
+      // Add event listener to the edit lyric form submit button
+      const editLyricForm = document.querySelector('#edit-lyric-form');
+      editLyricForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // Prevent form from submitting via browser
+        const id = document.querySelector('#edit-lyric-id').value;
+        const musicTitle = document.querySelector('#edit-music-title').value;
+        const artist = document.querySelector('#edit-artist').value;
+        const lyrics = document.querySelector('#edit-lyrics').value;
+            // Send PUT request to server to update lyric data
+    fetch(`https://allyricsbackend-production.up.railway.app/music/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        music_title: musicTitle,
+        artist: artist,
+        lyrics: lyrics
+      })
     })
-  })
-  .then(response => {
-    if (response.ok) {
-      // Reload lyrics table with updated data
-      loadLyricsTable();
-      // Hide the edit lyric form
-      document.querySelector('#edit-lyric-form').classList.add('d-none');
+    .then(response => {
+      if (response.ok) {
+        // Reload lyrics table with updated data
+        loadLyricsTable();
+        // Hide the edit lyric form
+        document.querySelector('#edit-lyric-form').classList.add('d-none');
+        Swal.fire({
+          title: 'Success!',
+          text: 'Lyric updated successfully!',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
+      } else {
+        throw new Error('Error updating lyric.');
+      }
+    })
+    .catch(error => {
+      console.error(error);
       Swal.fire({
-        title: 'Success!',
-        text: 'Lyric updated successfully!',
-        icon: 'success',
+        title: 'Error!',
+        text: error.message,
+        icon: 'error',
         confirmButtonText: 'OK'
       });
-    } else {
-      throw new Error('Error updating lyric.');
-    }
-  })
-  .catch(error => {
-    console.error(error);
-    Swal.fire({
-      title: 'Error!',
-      text: error.message,
-      icon: 'error',
-      confirmButtonText: 'OK'
     });
   });
-});
 
-// Add event listener to the cancel edit button
-const cancelEditButton = document.querySelector('#cancel-edit-btn');
-cancelEditButton.addEventListener('click', () => {
-  // Hide the edit lyric form
-  document.querySelector('#edit-lyric-form').classList.add('d-none');
-});
-
-});}
+  // Add event listener to the cancel edit button
+  const cancelEditButton = document.querySelector('#cancel-edit-btn');
+  cancelEditButton.addEventListener('click', () => {
+    // Hide the edit lyric form
+    document.querySelector('#edit-lyric-form').classList.add('d-none');
+  });
+// Load lyrics table when the page loads
+loadLyricsTable();
+    })
+}
