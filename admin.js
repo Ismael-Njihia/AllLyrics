9923ea457a20f31a9171ba1fbc8c1d2eba065c9f
reddangerous <dkinyanjui144@gmail.com>
2023-03-31 15:25:23 +0300

    updated

--- admin.js
+++ admin.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const addUserForm = document.querySelector('#add-user-form');
+  const editUserBtn = document.querySelector('#edit-user-btn');
+
   addUserForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting via the browser
     const formData = new FormData(addUserForm); // Serialize form data
@@ -20,38 +22,59 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(error => console.error(error));
   });
-});
-
- document.addEventListener('DOMContentLoaded', function() {
-  const deleteButton = document.querySelector('#deleteButton');
-  deleteButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    const checkedRows = document.querySelectorAll('.delete-checkbox:checked');
-    if (checkedRows.length > 0) {
-      const emails = Array.from(checkedRows).map(row => row.closest('tr').querySelector('.email').textContent);
-      const formData = new FormData();
-      formData.append('emails', JSON.stringify(emails));
-      fetch('https://allyricsbackend-production.up.railway.app/users/{}',emails, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(Object.fromEntries(formData.entries()))
-      })
+  editUserBtn.addEventListener('click', function() {
+    const selectedRows = document.querySelectorAll('#user-table-body tbody .delete-checkbox:checked');
+    if (selectedRows.length !== 1) {
+      alert('Please select one row to edit.');
+      return;
+    }
+    const selectedRow = selectedRows[0];
+    const email = selectedRow.querySelector('.email').textContent;
+    fetch(`https://allyricsbackend-production.up.railway.app/users/${email}`)
       .then(response => {
         if (response.ok) {
-          $('#deleteUserModal').modal('hide'); // Hide the modal after successful submission
-          location.reload(); // Reload the page to see the changes
+          return response.json();
         } else {
           throw new Error('Error in response.');
         }
       })
+      .then(data => {
+        // Populate the form with the user details
+        const inputs = addUserForm.querySelectorAll('input');
+        inputs.forEach(input => {
+          const name = input.name;
+          if (name in data) {
+            input.value = data[name];
+          } else {
+            input.value = '';
+          }
+        });
+        const emailInput = addUserForm.querySelector('[name="email"]');
+        emailInput.readOnly = true; // Disable editing of email
+        $('#addUserModal').modal('show'); // Show the modal with the user details
+      })
       .catch(error => console.error(error));
-    } else {
-      alert('Please select at least one user to delete.');
-    }
   });
 });
+
+ // Add event listener to the delete button
+      const deleteButton = document.querySelector('#delete-selected-btn');
+      deleteButton.addEventListener('click', () => {
+        const selectedRows = document.querySelectorAll('#user-table-body tbody .delete-checkbox:checked');
+        selectedRows.forEach(selectedRow => {
+          const email = selectedRow.querySelector('.email').textContent;
+          fetch(`https://allyricsbackend-production.up.railway.app/users/${email}`, { method: 'DELETE' })
+            .then(response => {
+              if (response.ok) {
+                const row = selectedRow.closest('tr');
+                row.remove();
+              } else {
+                throw new Error('Error in response.');
+              }
+            })
+            .catch(error => console.error(error));
+        });
+      }); 
 
    document.addEventListener('DOMContentLoaded', function() {
   fetch('https://allyricsbackend-production.up.railway.app/users')
