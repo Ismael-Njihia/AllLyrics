d84b53343f7d44efaa0c2c1ce6b145e6b782aec0
reddangerous <dkinyanjui144@gmail.com>
2023-03-31 12:42:43 +0300

    login Update

--- login.js
+++ login.js
@@ -1,25 +1,19 @@
- document.addEventListener('DOMContentLoaded', function () {
-  const signupForm = document.querySelector('#loginForm');
-  signupForm.addEventListener('submit', function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.querySelector('#loginForm');
+  loginForm.addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the form from submitting via the browser
-    const email = document.getElementById('email')
-    const password = document.getElementById('password') // Serialize form data
-    fetch('https://allyricsbackend-production.up.railway.app/${email}', {
-      method: 'POST',
-      headers: {
-        'content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: email.value,
-        password: password.value
-        
-      })
-      
-    })
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    fetch(`https://allyricsbackend-production.up.railway.app/users/${email.value}`)
       .then(response => {
-      if (response.ok) {
-        response.json().then(data => {
-          console.log(data);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Email not found.');
+        }
+      })
+      .then(data => {
+        if (data.password === password.value) {
           if (data.role === 'admin') {
             window.location.href = 'admin.html'; // Redirect to admin dashboard for admin users
           } else {
@@ -34,21 +28,18 @@
               }
             });
           }
+        } else {
+          throw new Error('Incorrect password.');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        Swal.fire({
+          title: 'Error!',
+          text: error.message,
+          icon: 'error',
+          confirmButtonText: 'OK'
         });
-      } else 
-      {
-        throw new Error('Error in response.');
-
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      Swal.fire({
-        title: 'Error!',
-        text: error.message,
-        icon: 'error',
-        confirmButtonText: 'OK'
       });
-    });
   });
 });
