198ca160c43c7b604546524de0ace75ef1d06172
reddangerous <dkinyanjui144@gmail.com>
2023-03-31 12:47:46 +0300

    updated login.js

--- login.js
+++ login.js
@@ -4,32 +4,34 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault(); // Prevent the form from submitting via the browser
     const email = document.getElementById('email');
     const password = document.getElementById('password');
-    fetch(`https://allyricsbackend-production.up.railway.app/users/${email.value}`)
+    fetch('https://allyricsbackend-production.up.railway.app/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
       .then(response => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error('Email not found.');
+          throw new Error('Incorrect email or password.');
         }
       })
       .then(data => {
-        if (data.password === password.value) {
-          if (data.role === 'admin') {
-            window.location.href = 'admin.html'; // Redirect to admin dashboard for admin users
-          } else {
-            Swal.fire({
-              title: 'Success!',
-              text: 'Login successful!',
-              icon: 'success',
-              confirmButtonText: 'OK'
-            }).then((result) => {
-              if (result.isConfirmed) {
-                window.location.href = 'index.html'; // Redirect to home page for non-admin users
-              }
-            });
-          }
+        console.log(data);
+        if (data.role === 'admin') {
+          window.location.href = 'admin.html'; // Redirect to admin dashboard for admin users
         } else {
-          throw new Error('Incorrect password.');
+          Swal.fire({
+            title: 'Success!',
+            text: 'Login successful!',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = 'index.html'; // Redirect to home page for non-admin users
+            }
+          });
         }
       })
       .catch(error => {
