d35b1bfd0d9c537a7e17a05346c0060b984daf2e
reddangerous <dkinyanjui144@gmail.com>
2023-03-31 13:21:26 +0300

    i

--- add-lyrics.js
+++ add-lyrics.js
@@ -7,7 +7,7 @@ addLyricForm.addEventListener('submit', event => {
   const requestBody = Object.fromEntries(formData.entries());
   requestBody.id = `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`; // generate a unique id
 
-  fetch('http://localhost:12345/music', {
+  fetch('https://allyricsbackend-production.up.railway.app/music', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(requestBody),
